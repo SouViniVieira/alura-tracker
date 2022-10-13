e9ffcf12566638d72edf5d88562b3b4572405d1c
SouViniVieira <vinicius1535@gmail.com>
2022-10-12 23:31:15 -0300

    V1 - AluraTracker

---
+++ src/components/FormularioPrinicipal.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulário para criação de uma nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa você deseja iniciar?"
+          v-model="descricao"
+        />
+      </div>
+      <div class="column">
+        <Temporizador @aoTemporizadorFinalizado="finalizarTarefa" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Temporizador from "./Temporizador.vue";
+
+export default defineComponent({
+  components: {
+    Temporizador,
+  },
+  data() {
+    return {
+      descricao: "",
+    };
+  },
+  methods: {
+    finalizarTarefa(tempoDecorrido: number): void {
+      console.log("Tempo da tarefa", tempoDecorrido);
+      console.log("Descrição da Tarefa", this.descricao);
+      this.descricao = ''
+    },
+  },
+});
+</script>
